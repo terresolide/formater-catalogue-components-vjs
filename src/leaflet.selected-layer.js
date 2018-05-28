b344ea2c4eb00bcc9695882cf5b868e158c9734a
epointal <elisabeth.pointal@free.fr>
2018-05-28 16:22:54 +0200

    #51 ajout credentials

--- src/leaflet.selected-layer.js
+++ src/leaflet.selected-layer.js
@@ -220,7 +220,7 @@ L.SelectedLayer =   L.Evented.extend({
 		obs.process.status = "WAITING";
 		var xhttp = new XMLHttpRequest(); 
 		xhttp.responseType = "json";
-		
+		xhttp.withCredentials = true;
 		xhttp.onreadystatechange = function() {
 			if (this.readyState == 4 && this.status == 200) {
 				//console.log( JSON.parse(this.responseText));
