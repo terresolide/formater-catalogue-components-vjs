6a554b0b8d59460101c6ef691f1356c2fa97926d
epointal <elisabeth.pointal@free.fr>
2018-10-19 16:17:51 +0200

    dependency to formater-tools instead of local plugin

--- src/main.js
+++ src/main.js
@@ -12,8 +12,8 @@ Vue.use(VueI18n);
 import VueResource from 'vue-resource';
 Vue.use(VueResource);
 
-// @todo voir pourquoi ne trouve pas ftTools quand directement
-import ftTools from 'formater-commons-components-vjs/src/formater-tools.js'
+import {VueTools} from 'formater-commons-components-vjs'
+Vue.use(VueTools)
 
 import AerisTheme from 'aeris-commons-components-vjs/src/aeris-theme/aeris-theme.vue'
  // import FormaterSpatialSearch from './formater-spatial-search.vue';
@@ -27,18 +27,18 @@ import AerisTheme from 'aeris-commons-components-vjs/src/aeris-theme/aeris-theme
 Vue.use(IsgiTools);*/
 
 // @todo supprimer passer autrement
-const formaterPlugin = {
-        install(Vue, options) {
-            Vue.prototype.$shadeColor= function(color, percent) {  
-                     var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
-                     // return "red";
-                     return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
-            }
-            
-        }
-};
-
-Vue.use( formaterPlugin);
+//const formaterPlugin = {
+//        install(Vue, options) {
+//            Vue.prototype.$shadeColor= function(color, percent) {  
+//                     var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+//                     // return "red";
+//                     return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+//            }
+//            
+//        }
+//};
+//
+//Vue.use( formaterPlugin);
 
 
 
